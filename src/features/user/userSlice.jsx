499a0af294fa26734110f83bbe542611cb399dcb
vijay4211 <vijayppandit2042@gmail.com>
2023-07-24 17:00:34 +0530

    Register User HTTP(AJAX)

--- src/features/user/userSlice.jsx
+++ src/features/user/userSlice.jsx
@@ -1,31 +1,53 @@
 import { createSlice, createAsyncThunk } from '@reduxjs/toolkit'
 import { toast } from 'react-toastify'
+import customFetch from '../../utils/axios'
 
 const initialState = {
   isLoading: false,
   user: null,
 }
 
-
 export const registerUser = createAsyncThunk(
   'user/registerUser',
   async (user, thunkAPI) => {
-    console.log(`Register User : ${JSON.stringify(user)}`)
+    try {
+      const resp = await customFetch.post('/auth/register', user);
+      // console.log("resp====",resp.data);
+      return resp.data
+    } catch (error) {
+      return thunkAPI.rejectWithValue(error.response.data.msg);
+      console.log("error.response=======",error.response)
+    }
   },
 )
 
 export const loginUser = createAsyncThunk(
-  'user/loginUser',
+  'user/registerUser',
   async (user, thunkAPI) => {
-    console.log(`Login User : ${JSON.stringify(user)}`)
+    console.log(`Login User: ${JSON.stringify(user)}`)
   },
 )
-
 
 const userSlice = createSlice({
   name: 'user',
   initialState,
+  extraReducers:(builder)=>{
+  builder
+  .addCase(registerUser.pending, (state)=>{
+    state.isLoading = true;
+  })
+  .addCase(registerUser.fulfilled, (state, {payload})=>{
+    const {user} = payload;
+    // console.log("payload=====",payload);
+    state.isLoading = false;
+    state.user = user;
+    toast.success(`Hello There ${user.name}`)
+  })
+  .addCase(registerUser.rejected, (state, {payload})=>{
+    state.isLoading = false;
+    toast.error(payload)
+  })
+  }
 })
 
 export default userSlice.reducer
-
--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -1,7 +1,7 @@
 import { useState, useEffect } from 'react'
 import { Logo, FormRow } from '../components'
 import Wrapper from '../assets/wrappers/RegisterPage'
-import {toast} from 'react-toastify'
+import { toast } from 'react-toastify'
 import { useDispatch, useSelector } from 'react-redux'
 import { loginUser, registerUser } from '../features/user/userSlice'
 
@@ -15,33 +15,31 @@ const initialState = {
 const Register = () => {
   const [values, setValues] = useState(initialState)
 
-  const {user, isLoading} = useSelector(store=> store.user);
-  const dispatch = useDispatch();
-
+  const { user, isLoading } = useSelector((store) => store.user)
+  const dispatch = useDispatch()
 
   const handleChange = (e) => {
-    const name = e.target.name;
-    const value = e.target.value;
-    setValues({...values, [name]:value});
+    const name = e.target.name
+    const value = e.target.value
+    setValues({ ...values, [name]: value })
   }
-
 
   const onSubmit = (e) => {
     e.preventDefault()
-    const {name,email,password,isMember} = values
-    if(!email || !password || (!isMember && !name)){
-      toast.error("Please fill out all fields");
-      return;
+    const { name, email, password, isMember } = values
+    if (!email || !password || (!isMember && !name)) {
+      toast.error('Please fill out all fields')
+      return
     }
-    if(isMember){
-      dispatch(loginUser({email:email, password:password})) 
-      return;
+    if (isMember) {
+      dispatch(loginUser({ email, password}))
+      return
     }
     dispatch(registerUser({name, email, password}))
   }
 
   const toggleMember = () => {
-    setValues({ ...values, isMember: !values.isMember }) 
+    setValues({ ...values, isMember: !values.isMember })
   }
 
   return (
@@ -73,15 +71,13 @@ const Register = () => {
           value={values.password}
           handleChange={handleChange}
         />
-        <button type="submit" className="btn btn-block">
+        <button type="submit" className="btn btn-block" disabled={isLoading}>
           submit
         </button>
         <p>
-          {
-            values.isMember ? "Not a member yet?" : "Already a member?"
-          }
-          <button type='button' onClick={toggleMember} className='member-btn'>
-            {values.isMember ? "Register" : "Login"}
+          {values.isMember ? 'Not a member yet?' : 'Already a member?'}
+          <button type="button" onClick={toggleMember} className="member-btn">
+            {values.isMember ? 'Register' : 'Login'}
           </button>
         </p>
       </form>
