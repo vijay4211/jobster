eb8b54ebad1a0e8f8784d2c7342e7b193bab7785
vijay4211 <vijayppandit2042@gmail.com>
2023-07-29 13:13:40 +0530

    Get all job Request complete

--- src/features/allJobs/allJobsSlice.jsx
+++ src/features/allJobs/allJobsSlice.jsx
@@ -22,9 +22,41 @@ const initialState = {
   monthlyApplications: [],
   ...initialFiltersState,
 }
+console.log(initialState);
+
+export const getAllJobs = createAsyncThunk(
+  'allJobs/getJobs',
+  async (_, thunkAPI) => {
+    let url = '/jobs'
+    try {
+      const resp = await customFetch.get(url, {
+        headers: {
+          authorization: `Bearer ${thunkAPI.getState().user.user.token}`,
+        },
+      })
+      return resp.data
+    } catch (error) {
+      return thunkAPI.rejectWithValue('There was an error')
+    }
+  },
+)
 
 const allJobSlice = createSlice({
   name: 'allJobs',
   initialState,
+  extraReducers: (build) => {
+    build
+      .addCase(getAllJobs.pending, (state) => {
+        state.isLoading = true
+      })
+      .addCase(getAllJobs.fulfilled, (state, { payload }) => {
+        state.isLoading = false
+        state.jobs = payload.jobs
+      })
+      .addCase(getAllJobs.rejected, (state, { payload }) => {
+        state.isLoading = false
+        toast.error(payload)
+      })
+  },
 })
 export default allJobSlice.reducer
